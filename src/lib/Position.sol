00bbbd983dd3cdb56eff11d0c9b1253e1ad131e0
何琦 <993284758@qq.com>
2023-08-21 20:56:48 +0800

    test: mint

--- src/lib/Position.sol
+++ src/lib/Position.sol
@@ -1,5 +1,6 @@
-// 跟踪现在的价格和对应的 tick。我们将会把他们存储在一个 slot 中来节省 gas 费
+pragma solidity ^0.8.14;
 
+// 跟踪现在的价格和对应的 tick。我们将会把他们存储在一个 slot 中来节省 gas 费
 library Position {
   struct Info {
     uint128 liquidity;
