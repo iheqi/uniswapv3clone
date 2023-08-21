c9e13dece8b1c90d47479fbef3bb0e55696af0d3
何琦 <993284758@qq.com>
2023-08-21 20:50:13 +0800

    feat: mint

--- src/UniswapV3Pool.sol
+++ src/UniswapV3Pool.sol
@@ -3,13 +3,28 @@ pragma solidity ^0.8.14;
 import './lib/Tick.sol';
 import './lib/Position.sol';
 
-contract UniswapV3Pool {
-  // 每个池子合约都包含一些 tick 的信息，需要一个 mapping 来存储 tick 的下标与对应的信息
-  using Tick for mapping(int24 => Tick.Info);
+import "./interfaces/IERC20.sol";
+import "./interfaces/IUniswapV3MintCallback.sol";
 
-  // 跟踪现在的价格和对应的 tick。我们将会把他们存储在一个 slot 中来节省 gas 费
+
+contract UniswapV3Pool {
+  using Tick for mapping(int24 => Tick.Info);
   using Position for mapping(bytes32 => Position.Info);
   using Position for Position.Info;
+
+  error InsufficientInputAmount();
+  error InvalidTickRange();
+  error ZeroLiquidity();
+
+  event Mint(
+      address sender,
+      address indexed owner,
+      int24 indexed tickLower,
+      int24 indexed tickUpper,
+      uint128 amount,
+      uint256 amount0,
+      uint256 amount1
+  );
 
   int24 internal constant MIN_TICK = -887272;
   int24 internal constant MAX_TICK = -MIN_TICK;
@@ -27,7 +42,10 @@ contract UniswapV3Pool {
 
   uint128 public liquidity;
 
+  // 每个池子合约都包含一些 tick 的信息，需要一个 mapping 来存储 tick 的下标与对应的信息
   mapping(int24 => Tick.Info) public ticks;
+
+  // 跟踪现在的价格和对应的 tick。我们将会把他们存储在一个 slot 中来节省 gas 费
   mapping(bytes32 => Position.Info) public positions;
 
 
@@ -43,4 +61,60 @@ contract UniswapV3Pool {
     slot0 = Slot0({ sqrtPriceX96: sqrtPriceX96, tick: tick });
   }
 
+  // mint 函数会包含以下参数：
+
+  // 1.token 所有者的地址，来识别是谁提供的流动性；
+  // 2.上界和下界的 tick，来设置价格区间的边界；
+  // 3.希望提供的流动性的数量
+  function mint(
+    address owner, 
+    int24 lowerTick, 
+    int24 upperTick, 
+    uint128 amount
+  ) external returns (uint256 amount0, uint256 amount1) {
+    
+    // 首先来检查 ticks:
+    if (
+      lowerTick >= upperTick ||
+        lowerTick < MIN_TICK ||
+        upperTick > MAX_TICK
+    ) revert InvalidTickRange();
+
+    // 并且确保提供的流动性数量不为零：
+    if (amount == 0) revert ZeroLiquidity();
+
+    // 在下界 tick 和上界 tick 处均调用此函数，流动性在两边都有添加
+    ticks.update(lowerTick, amount);
+    ticks.update(upperTick, amount);
+
+    Position.Info storage position = positions.get(owner, lowerTick, upperTick);
+    position.update(amount);
+
+    amount0 = 0.998976618347425280 ether; // TODO: replace with calculation
+    amount1 = 5000 ether; // TODO: replace with calculation
+
+    liquidity += uint128(amount);
+
+    uint256 balance0Before;
+    uint256 balance1Before;
+    if (amount0 > 0) balance0Before = balance0();
+    if (amount1 > 0) balance1Before = balance1();
+
+    IUniswapV3MintCallback(msg.sender).uniswapV3MintCallback(
+      amount0,
+      amount1
+    );
+
+    if (amount0 > 0 && balance0Before + amount0 > balance0()) revert InsufficientInputAmount();
+    if (amount1 > 0 && balance1Before + amount1 > balance1()) revert InsufficientInputAmount();
+    emit Mint(msg.sender, owner, lowerTick, upperTick, amount, amount0, amount1);
+  }
+
+  function balance0() internal returns (uint256 balance) {
+    balance = IERC20(token0).balanceOf(address(this));
+  }
+
+  function balance1() internal returns (uint256 balance) {
+    balance = IERC20(token1).balanceOf(address(this));
+  }
 }
--- src/lib/Position.sol
+++ src/lib/Position.sol
@@ -4,4 +4,21 @@ library Position {
   struct Info {
     uint128 liquidity;
   }
+
+  function update(Info storage self, uint128 liquidityDelta) internal {
+    uint128 liquidityBefore = self.liquidity;
+    uint128 liquidityAfter = liquidityBefore + liquidityDelta;
+    self.liquidity = liquidityAfter;
+  }
+
+  // 每个位置都由三个变量所确定：LP 地址，下界 tick 下标，上界 tick 下标。
+  // 我们将这三个变量哈希来减少数据存储开销：哈希结果只有 32 字节，而三个变量分别存储需要 96 字节
+  function get(
+    mapping(bytes32 => Info) storage self,
+    address owner,
+    int24 lowerTick,
+    int24 upperTick
+  ) internal view returns (Position.Info storage position) {
+    position = self[keccak256(abi.encodePacked(owner, lowerTick, upperTick))];
+  }
 }
